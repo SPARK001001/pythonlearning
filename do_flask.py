f3e26f132aeb1ac496b284f93c37c3530760fbe2
pengjianrong <pengjianrong@linkedsee.com>
2018-04-28 18:35:56 +0800

    some note

---
+++ do_flask.py
@@ -0,0 +1,24 @@
+#_*_coding:utf-8_*_
+from flask import Flask,render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET', 'POST'])
+def home():
+	return render_template('home_html')
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+	return render_template('form_html')
+
+@app.route('/signin',methods=['POST'])
+def signin():
+	username = request.form('username')
+	password = request.form('password')
+	if username=='admin' and password=='password':
+		return render_template('signin_ok.html',username=username)
+	return render_template('form.html',message='Bad username or password',username=username)
+
+if __name__ == '__main__':
+	app.run()
